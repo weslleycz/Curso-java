391876048d0213eb1349b9bcfab6f33be1464b2d
Weslley <weslleysouza22@gmail.com>
2022-11-08 22:36:11 -0300

    Convertendo data-hora global para local

---
+++ 112. Convertendo data-hora global para local/src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Main {
+    public static void main(String[] args) {
+        LocalDate d04 = LocalDate.parse("2022-07-20");
+        LocalDateTime d05 = LocalDateTime.parse("2022-07-20T01:30:26");
+        Instant d06 = Instant.parse("2022-07-20T01:30:26Z");
+        DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+    }
+}
